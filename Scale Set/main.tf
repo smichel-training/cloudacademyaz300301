2cc47bc3f1f3b392b1567fd7c6b8d6633b10d54e
Michel Sebastien <smichel@devoteam.com>
2020-02-08 18:14:59 +0100

    added scales rules based on CPU

--- Scale Set/main.tf
+++ Scale Set/main.tf
@@ -184,8 +184,72 @@ resource "azurerm_virtual_machine_scale_set" "example" {
         project = var.ProjectName
         version = var.BuildVersion
     }
-
  }
+
+ resource "azurerm_monitor_autoscale_setting" "example" {
+  name                          = "myAutoscaleSetting"
+  resource_group_name           = azurerm_resource_group.myterraformgroup.name
+  location                      = var.location
+  target_resource_id            = azurerm_virtual_machine_scale_set.example.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 2
+      minimum = 2
+      maximum = 6
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.example.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 80
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.example.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+
+  notification {
+    email {
+      send_to_subscription_administrator    = true
+      send_to_subscription_co_administrator = true
+      custom_emails                         = ["aix.smichel@gmail.com"]
+    }
+  }
+}
 
 
 
