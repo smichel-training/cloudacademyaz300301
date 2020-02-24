d0264a657e03e18c885fd733772adb6509eeb06b
Michel Sebastien <smichel@devoteam.com>
2020-02-24 14:22:25 +0100

    Azure storage scripts working

--- AzureStorage Powershell/Lab-functions.ps1
+++ AzureStorage Powershell/Lab-functions.ps1
@@ -1,28 +1,82 @@
 # Function Add-Entity: Adds a blob file entity of a given type to a table.
 function Add-Entity() {
-    [CmdletBinding()]
-    param(
-      $Table,
-      $Blob,
-      $Type
-    )
+  [CmdletBinding()]
+  param(
+    $Table,
+    $Blob,
+    $Type
+  )
   
-    # Use the Container name as the partition key
-    $PartitionKey = $Blob.ICloudBlob.Container.Name
-    # Use the blob name as the row key
-    $RowKey = $Blob.Name
+  # Use the Container name as the partition key
+  $PartitionKey = $Blob.ICloudBlob.Container.Name
+  # Use the blob name as the row key
+  $RowKey = $Blob.Name
 
-    # https://github.com/paulomarquesc/AzureRmStorageTable/tree/master/docs
-    Add-AzTableRow -table $Table -partition $partitionKey -rowKey $RowKey -property @{"Type"=$Type;"storageUri"=$Blob.ICloudBlob.StorageUri.PrimaryUri.AbsoluteUri}
+  # https://github.com/paulomarquesc/AzureRmStorageTable/tree/master/docs
+  Add-AzTableRow -table $Table -partition $partitionKey -rowKey $RowKey -property @{"Type" = $Type; "storageUri" = $Blob.ICloudBlob.StorageUri.PrimaryUri.AbsoluteUri }
+}
 
-
+# Add message to a queue
+function Add-Message() {
+  [CmdletBinding()]
+  param(
+    $Queue,
+    $Blob
+  )
+  # Generate read-only SAS token for the blob
+  $Sas = New-AzStorageBlobSASToken -CloudBlob $Blob.ICloudBlob -Permission r
   
-    #$Entity = New-Object -TypeName Microsoft.WindowsAzure.Storage.Table.DynamicTableEntity
-    #$Entity.PartitionKey = $PartitionKey
-    #$Entity.RowKey = $RowKey
-    #$Entity.Properties.Add("Type", $Type)
-    #$Entity.Properties.Add("StorageUri", $Blob.ICloudBlob.StorageUri.PrimaryUri.AbsoluteUri)
-  
-    #$Table.CloudTable.Execute([Microsoft.WindowsAzure.Storage.Table.TableOperation]::Insert($Entity))
-    
+  # Message contains blob StorageUri and Name (what is needed to generate the thumbnail)
+  $MessageHashTable = @{ 
+      StorageUri                      = $Blob.ICloudBlob.StorageUri.PrimaryUri;
+      Sas                             = $Sas;
+      Name                            = "$($Blob.ICloudBlob.Container.Name)\$($Blob.Name)" 
   }
+  
+  # Convert the hash table to a JSON string
+  $Message = ConvertTo-Json -InputObject $MessageHashTable
+  
+  # Construct a CloudQueueMessage with the serialized message
+  $QueueMessage = New-Object -TypeName Microsoft.Azure.Storage.Queue.CloudQueueMessage -ArgumentList $Message
+  
+  # Add the message to the queue
+  $Queue.CloudQueue.AddMessage($QueueMessage)
+}
+
+# Process a message if one is available and remove it when complete
+function Process-Message() {
+  [CmdletBinding()]
+  param(
+    $Queue
+  )
+  
+  # Set the number of seconds to make a queue message invisible while processing
+  $InvisibleTimeout = [System.TimeSpan]::FromSeconds(10)
+  
+  # Attempt to get a message from the queue
+  $QueueMessage = $Queue.CloudQueue.GetMessage($InvisibleTimeout)
+  
+  if ($QueueMessage -eq $null) { 
+    Write-Host "Empty queue"
+    return
+  }
+  
+  # Deserialize the JSON message
+  $MessageHashTable = ConvertFrom-Json -InputObject $QueueMessage.AsString
+  
+  # Download the image to a temp file
+  $TempFile = [System.IO.Path]::GetTempFileName()
+  Invoke-WebRequest -Uri "$($MessageHashTable.StorageUri)$($MessageHashTable.Sas)" -OutFile $TempFile
+  
+  # Resize the image
+  $ThumbnailFile = "$($TempFile)_thumb"
+  Resize-Image -InputFile $TempFile -OutputFile $ThumbnailFile -Height 100 -Width 300
+  
+  # Upload the thumbnail to the thumbnails container
+  Set-AzStorageBlobContent -File $ThumbnailFile -Container thumbnails -Blob $MessageHashTable.Name
+  
+  Write-Host "Finished Processing"
+  
+  # Delete the message from the queue
+  $Queue.CloudQueue.DeleteMessage($QueueMessage)
+}
--- AzureStorage Powershell/main.ps1
+++ AzureStorage Powershell/main.ps1
@@ -1,16 +1,24 @@
-# Connect to your Cloud Academy temporary Azure account
+# Required components
+# Powershell AZ module
+# Powershell AzTable module (Install-Module -Name AzTable)
+# Microsoft.azure.storage.queue type
+add-type -Path 'C:\Users\smichel\Documents\PowerShell\Modules\Az.Storage\1.11.0\Microsoft.Azure.Storage.Queue.dll'
 
-# get the lab resource group name
-$rg = $(Get-AzResourceGroup).ResourceGroupName
+# Connect to your Cloud Academy temporary Azure account
+# Connect-AzAccount
+
+# get the lab resource group name or if you are working on your account then create a dedicated resource group
+# $rg = $(Get-AzResourceGroup).ResourceGroupName
+$rg = New-AzResourceGroup -Name azstoragetraining -Location 'West Europe'
 
 # create a new storage account based on storageTemplate.json file
-New-AzResourceGroupDeployment -ResourceGroupName $rg -Name storage-deployment -TemplateFile .\storageTemplate.json
+New-AzResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -Name storage-deployment -TemplateFile .\storageTemplate.json
 
 # get the newly created storage account name
-$createdStorageAccount = $(Get-AzResourceGroupDeployment -ResourceGroupName $rg -DeploymentName storage-deployment).Outputs.storageAccountName.value
+$createdStorageAccount = $(Get-AzResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -DeploymentName storage-deployment).Outputs.storageAccountName.value
 
 # Make the new storage account the default for the current PowerShell session
-Set-AzCurrentStorageAccount -ResourceGroupName $rg -StorageAccountName $createdStorageAccount
+Set-AzCurrentStorageAccount -ResourceGroupName $rg.ResourceGroupName -StorageAccountName $createdStorageAccount
 
 # download image
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cloudacademy/azure-lab-provisioners/master/azure-storage/image.png" -OutFile .\image.png
@@ -76,5 +84,51 @@ $Wallpaper = Get-AzStorageBlob -Container wallpapers
 Add-Entity -Table $Tbl.CloudTable -Blob $Logo -Type "logo"
 Add-Entity -Table $Tbl.CloudTable -Blob $Wallpaper -Type "wallpaper"
 
+# Query the table
+# Default query is "Select all"
+Get-AzTableRow -table $tbl.CloudTable | ft
 
 
+# Get entity from Wallpapers Partition Key
+# Partiton Key is case sensitive !!
+$entity = Get-AzTableRow -table $tbl.CloudTable -partitionKey 'wallpapers'
+
+# Get entity based on the type collumn
+# columnName and value are case sensitive
+$entity = get-azTableRow -table $tbl.CloudTable -columnName "Type" -value "wallpaper" -operator Equal
+
+# Display entity
+$Entity
+
+# Create a message Queue
+$QueueName = "thumbnail-queue" 
+$Queue = New-AzStorageQueue –Name $QueueName
+
+# Add Messages to the queue
+Add-Message -Queue $Queue -Blob $Logo
+Add-Message -Queue $Queue -Blob $Wallpaper
+
+# Refresh $Queue object
+$Queue = Get-AzStorageQueue -Name $QueueName
+$Queue.ApproximateMessageCount
+
+# download and import Resize-Image module
+Invoke-WebRequest -Uri "https://gallery.technet.microsoft.com/scriptcenter/Resize-Image-A-PowerShell-3d26ef68/file/135684/1/Resize-Image.psm1" -OutFile .\Resize-Image.psm1
+Import-Module .\Resize-Image.psm1 
+
+# Create a new blob container
+New-AzStorageContainer -Name thumbnails -Permission Container
+
+# Process Message in queue
+Process-Message -Queue $Queue
+Process-Message -Queue $Queue
+Process-Message -Queue $Queue
+
+# Get Storage Account Key
+Get-AzStorageAccountKey -ResourceGroupName $rg.ResourceGroupName -Name $createdStorageAccount
+# Regenerate key1
+New-AzStorageAccountKey -ResourceGroupName $rg.ResourceGroupName -Name $createdStorageAccount -KeyName key1
+# Regenerate key2 by entering:
+New-AzStorageAccountKey -ResourceGroupName $rg.ResourceGroupName -Name $createdStorageAccount -KeyName key2
+# List the keys to verify they are not the same as before:
+Get-AzStorageAccountKey -ResourceGroupName $rg.ResourceGroupName -Name $createdStorageAccount
